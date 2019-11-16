9b9a20c2e8830f789b2b350b3a1805a03c742eb5
Stanyke <chibuike.stanley50@gmail.com>
2019-11-16 23:50:50 +0100

    Updated create user route to enable just admin to create new accounts

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -107,11 +107,17 @@ app.post('/auth/create-user', function (req, response)
         jobRole: req.body.jobRole,
         department: req.body.department,
         address: req.body.address,
-        isAdmin: req.body.isAdmin
+        isAdmin: req.body.isAdmin,
+        verifyAdmin: req.body.verifyAdmin
     };
 
-    client.query(`SELECT email FROM users WHERE email ='${req.body.email}'`, (err, res) => 
+    client.query(`SELECT * FROM users WHERE isAdmin ='1' AND email ='${req.body.verifyAdmin}'`, (err, res) => 
     {
+        if (!res.rows[0])
+        {
+            response.status(500).send("You're Not Authorized To Create New Users, Just Admins Are Allowed...");
+        }
+
         if (err)
         {
             console.log('We Encountered An Internal Error, Try Again...');
@@ -120,35 +126,49 @@ app.post('/auth/create-user', function (req, response)
 
         if (res.rows[0])
         {
-            console.log(req.body.email, "Already Exist");
-            return response.send(`${req.body.email} Already Exist`);
-        }
-
-        
-        bcrypt.hash(req.body.password, 10, function (error, hash)
-        {
-            if (error)
-            {
-                console.log('We Encountered an issue processing your Account');
-                response.status(500).send('We Encountered an issue processing your Account');
-            }
-            
-            const realvalues = [data.firstname, data.lastname, data.email, hash, data.gender, data.jobRole, data.department, data.address, data.isAdmin];
-
-            client.query('INSERT INTO users (firstname, lastname, email, password, gender, jobRole, department, address, isAdmin) values($1, $2, $3, $4, $5, $6, $7, $8, $9)', realvalues, (derr, result) =>
+            client.query(`SELECT email FROM users WHERE email ='${req.body.email}'`, (derr, myres) => 
             {
                 if (derr)
                 {
-                    console.log('We Encountered an issue creating your Account');
-                    response.status(500).send('We Encountered an issue creating your Account');
+                    console.log('We Encountered An Issue Proccessing This');
+                    response.status(500).send('We Encountered An Issue Proccessing This');
                 }
-                if (result)
+
+                if (myres.rows[0])
                 {
-                response.status(201).send(`${data.firstname}'s Account Created`);
-                console.log(`${data.firstname}'s Account Created`);
+                    console.log(req.body.email, "Already Exist");
+                    response.send(`${req.body.email} Already Exist`);
+                }
+
+                if (!myres.rows[0])
+                {
+                    bcrypt.hash(req.body.password, 10, function (error, hash)
+                    {
+                        if (error)
+                        {
+                            console.log('We Encountered an issue processing your Account');
+                            response.status(500).send('We Encountered an issue processing your Account');
+                        }
+                        
+                        const realvalues = [data.firstname, data.lastname, data.email, hash, data.gender, data.jobRole, data.department, data.address, data.isAdmin];
+
+                        client.query('INSERT INTO users (firstname, lastname, email, password, gender, jobRole, department, address, isAdmin) values($1, $2, $3, $4, $5, $6, $7, $8, $9)', realvalues, (uerr, result) =>
+                        {
+                            if (uerr)
+                            {
+                                console.log('We Encountered an issue creating your Account');
+                                response.status(500).send('We Encountered an issue creating your Account');
+                            }
+                            if (result)
+                            {
+                            response.status(201).send(`${data.firstname}'s Account Created`);
+                            console.log(`${data.firstname}'s Account Created`);
+                            }
+                        });
+                    });
                 }
             });
-        });
+        }
     });
 });
 
